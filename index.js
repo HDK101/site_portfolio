5bc05848b496632f7f14c72c4f4b936ea9935549
Lucas Hideki <lucashideki87@gmail.com>
2020-01-18 13:07:23 -0300

    Switched render to renderFile

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const ejs = require("ejs");
 const fs = require("fs");
 
-html = ejs.render("<h1>Hello there</h1>");
-console.log(html);
+let html = "";
+// html = ejs.render("<h1>Hello there</h1>");
+ejs.renderFile("./views/index.ejs", function(err, str) {
+  if(err) throw err;
+  html = str;
+});
 
 fs.writeFile("index.html", html, function(err) {
   if (err) throw err;
